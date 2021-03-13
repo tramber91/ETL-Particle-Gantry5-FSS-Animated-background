819cc86f3c0661bfc7b1b19981ae327646773146
En Toutes Lettres <tramber91@neuf.fr>
2021-03-13 13:26:19 +0100

    Add files via upload

---
+++ js/fss-config-yellow.js
@@ -0,0 +1,42 @@
+  //------------------------------
+  // Mesh Properties
+  //------------------------------
+  var MESH = {
+    width: 1.1,
+    height: 1.1,
+    depth: 15,
+    segments: 12,
+    slices: 15,
+    xRange: 0.5,
+    yRange: 0.2,
+    zRange: 1.0,
+    ambient: '#555555',
+    diffuse: '#ffffff',
+    speed: 0.0006
+};
+
+  //------------------------------
+  // Light Properties
+  //------------------------------
+  var LIGHT = {
+    count: 2,
+    xyScalar: 1,
+    zOffset: 200,
+    ambient: '#7A5E00',
+    diffuse: '#D4A200',
+    speed: 0.0004,
+    gravity: 6000,
+    dampening: 0.001,
+    minLimit: 10,
+    maxLimit: null,
+    minDistance: 20,
+    maxDistance: 400,
+    autopilot: true,
+    draw: false,
+    bounds: FSS.Vector3.create(),
+    step: FSS.Vector3.create(
+        Math.randomInRange(0.2, 1.0),
+        Math.randomInRange(0.2, 1.0),
+        Math.randomInRange(0.2, 1.0)
+    )
+};
